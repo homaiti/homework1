f8a5db99aaba400e702ef74dce6b95b3b018242a
Mikhail Yakovlev <homaiti@gmail.com>
2023-01-20 13:17:05 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Привет, мир");
+        System.out.println("Помогите Даше найти галку на удаление бранча");
     }
 }
